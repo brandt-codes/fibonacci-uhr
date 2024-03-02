fcd038c8bd39024a33bc4b10c93dcb3cae0bd083
Alexander Brandt <alex@brandt.codes>
2024-03-02 15:22:46 +0100

    feat: mapping for fibonacci clock elements

--- src/app/clock/clock.component.spec.ts
+++ src/app/clock/clock.component.spec.ts
@@ -2,9 +2,202 @@ import { ClockComponent } from './clock.component';
 
 describe('ClockComponent', () => {
   let component: ClockComponent;
+  beforeEach(() => {
+    component = new ClockComponent();
+  });
 
   it('should create', () => {
-    component = new ClockComponent();
     expect(component).toBeTruthy();
+  });
+
+  describe('Hours Mapping', () => {
+    it('should set "hours" for element of first 1 when 1 is given', () => {
+      component.setHour(1);
+      expect(component.activeHourMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "hours" for element of 2 when 2 is given', () => {
+      component.setHour(2);
+      expect(component.activeHourMap.get('2')).toBe(true);
+    });
+
+    it('should set "hours" for element of 3 when 3 is given', () => {
+      component.setHour(3);
+      expect(component.activeHourMap.get('3')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5 when 5 is given', () => {
+      component.setHour(4);
+      expect(component.activeHourMap.get('3')).toBe(true);
+      expect(component.activeHourMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5 when 5 is given', () => {
+      component.setHour(5);
+      expect(component.activeHourMap.get('5')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5 and 1 when 6 is given', () => {
+      component.setHour(6);
+      expect(component.activeHourMap.get('5')).toBe(true);
+      expect(component.activeHourMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5 and 2 when 7 is given', () => {
+      component.setHour(7);
+      expect(component.activeHourMap.get('5')).toBe(true);
+      expect(component.activeHourMap.get('2')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5 and 3 when 8 is given', () => {
+      component.setHour(8);
+      expect(component.activeHourMap.get('5')).toBe(true);
+      expect(component.activeHourMap.get('3')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5, 3 and 1 when 9 is given', () => {
+      component.setHour(9);
+      expect(component.activeHourMap.get('5')).toBe(true);
+      expect(component.activeHourMap.get('3')).toBe(true);
+      expect(component.activeHourMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5, 3 and 2 when 10 is given', () => {
+      component.setHour(10);
+      expect(component.activeHourMap.get('5')).toBe(true);
+      expect(component.activeHourMap.get('3')).toBe(true);
+      expect(component.activeHourMap.get('2')).toBe(true);
+    });
+
+    it('should set "hours" for element of 5, 3, 2 and first 1 when 11 is given', () => {
+      component.setHour(11);
+      expect(component.activeHourMap.get('5')).toBe(true);
+      expect(component.activeHourMap.get('3')).toBe(true);
+      expect(component.activeHourMap.get('2')).toBe(true);
+      expect(component.activeHourMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "hours" for all elements when 12 is given', () => {
+      component.setHour(12);
+      expect(component.activeHourMap.get('5')).toBe(true);
+      expect(component.activeHourMap.get('3')).toBe(true);
+      expect(component.activeHourMap.get('2')).toBe(true);
+      expect(component.activeHourMap.get('1.1')).toBe(true);
+      expect(component.activeHourMap.get('1.2')).toBe(true);
+    });
+
+    it('should not set any "hour" when 0 is given', () => {
+      component.setHour(0);
+      expect(component.activeHourMap.get('1.1')).toBe(undefined);
+      expect(component.activeHourMap.get('1.2')).toBe(undefined);
+      expect(component.activeHourMap.get('2')).toBe(undefined);
+      expect(component.activeHourMap.get('3')).toBe(undefined);
+      expect(component.activeHourMap.get('5')).toBe(undefined);
+    });
+  });
+
+
+  describe('Minutes Mapping', () => {
+    it('should set "minutes" of first "1" when 5 Minutes is given', () => {
+      component.setMinute(5);
+      expect(component.activeMinuteMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "minutes" for 2 when 10 Minutes is given', () => {
+      component.setMinute(10);
+      expect(component.activeMinuteMap.get('2')).toBe(true);
+    });
+
+    it('should set "minutes" for 3 when 15 Minutes is given', () => {
+      component.setMinute(15);
+      expect(component.activeMinuteMap.get('3')).toBe(true);
+    });
+
+    it('should set "minutes" for 3 and 1 when 20 Minutes is given', () => {
+      component.setMinute(20);
+      expect(component.activeMinuteMap.get('3')).toBe(true);
+      expect(component.activeMinuteMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "minutes" for 5 when 25 Minutes is given', () => {
+      component.setMinute(25);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+    });
+
+    it('should set "minutes" for 5 and 1 when 30 Minutes is given', () => {
+      component.setMinute(30);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+      expect(component.activeMinuteMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "minutes" for 5 and 2 when 35 Minutes is given', () => {
+      component.setMinute(35);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+      expect(component.activeMinuteMap.get('2')).toBe(true);
+    });
+
+    it('should set "minutes" for 5 and 3 when 40 Minutes is given', () => {
+      component.setMinute(40);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+      expect(component.activeMinuteMap.get('3')).toBe(true);
+    });
+
+    it('should set "minutes" for 5, 3 and 1 when 45 Minutes is given', () => {
+      component.setMinute(45);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+      expect(component.activeMinuteMap.get('3')).toBe(true);
+      expect(component.activeMinuteMap.get('1.1')).toBe(true);
+    });
+
+    it('should set "minutes" for 5, 3 and 2 when 50 Minutes is given', () => {
+      component.setMinute(50);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+      expect(component.activeMinuteMap.get('3')).toBe(true);
+      expect(component.activeMinuteMap.get('2')).toBe(true);
+    });
+
+    it('should set "minutes" for 5, 3 and 2 when 55 Minutes is given', () => {
+      component.setMinute(55);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+      expect(component.activeMinuteMap.get('3')).toBe(true);
+      expect(component.activeMinuteMap.get('2')).toBe(true);
+      expect(component.activeMinuteMap.get('1.1')).toBe(true);
+    });
+
+    it('should ignore invalid number: 11', () => {
+      component.setMinute(11);
+      expect(component.activeMinuteMap.get('1.1')).toBe(undefined);
+      expect(component.activeMinuteMap.get('1.2')).toBe(undefined);
+      expect(component.activeMinuteMap.get('2')).toBe(undefined);
+      expect(component.activeMinuteMap.get('3')).toBe(undefined);
+      expect(component.activeMinuteMap.get('5')).toBe(undefined);
+    });
+
+    it('should not set any field when 0 is given', () => {
+      component.setMinute(0);
+      expect(component.activeMinuteMap.get('1.1')).toBe(undefined);
+      expect(component.activeMinuteMap.get('1.2')).toBe(undefined);
+      expect(component.activeMinuteMap.get('2')).toBe(undefined);
+      expect(component.activeMinuteMap.get('3')).toBe(undefined);
+      expect(component.activeMinuteMap.get('5')).toBe(undefined);
+    });
+
+    it('should not set all active if 60 minutes given', () => {
+      component.setMinute(60);
+      expect(component.activeMinuteMap.get('1.1')).toBe(true);
+      expect(component.activeMinuteMap.get('1.2')).toBe(true);
+      expect(component.activeMinuteMap.get('2')).toBe(true);
+      expect(component.activeMinuteMap.get('3')).toBe(true);
+      expect(component.activeMinuteMap.get('5')).toBe(true);
+    });
+
+    it('should not set any field when 65 is given', () => {
+      component.setMinute(65);
+      expect(component.activeMinuteMap.get('1.1')).toBe(undefined);
+      expect(component.activeMinuteMap.get('1.2')).toBe(undefined);
+      expect(component.activeMinuteMap.get('2')).toBe(undefined);
+      expect(component.activeMinuteMap.get('3')).toBe(undefined);
+      expect(component.activeMinuteMap.get('5')).toBe(undefined);
+    });
   });
 });
--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -11,5 +11,41 @@ import { ClockElementComponent } from './clock-element/clock-element.component';
   styleUrl: './clock.component.scss'
 })
 export class ClockComponent {
+  public activeHourMap:Map<string, boolean> = new Map();
+  public activeMinuteMap:Map<string, boolean> = new Map();
+  private numberToBoxMap: Map<number, string[]> = new Map([
+    [0, [""]],    // 0 hour or 0 minute
+    [1, ["1.1"]], // 1 hour or 5 minute
+    [2, ["2"]],   // 2 hour or 10 minute
+    [3, ["3"]],   // 3 hour or 15 minute
+    [4, ["3", "1.1"]],   // 4 hour or 20 minute
+    [5, ["5"]],   // 5 hour or 25 minute
+    [6, ["5", "1.1"]],    // 6 hour or 30 minute
+    [7, ["5", "2"]],    // 7 hour or 35 minute
+    [8, ["5", "3"]],    // 8 hour or 40 minute
+    [9, ["5", "3", "1.1"]],    // 9 hour or 45 minute
+    [10, ["5", "3", "2"]],    // 10 hour or 50 minute
+    [11, ["5", "3", "2", "1.1"]],    // 11 hour or 55 minute
+    [12, ["5", "3", "2", "1.1", "1.2"]], // 12 hours 60 minutes (what only makes sense without "minutes")
+  ]);
 
+  public setHour(hour: number): void {
+    this.activeHourMap.clear();
+    this.setMappings(hour, true);
+  }
+
+  public setMinute(minute: number): void {
+    const preparedMinute = minute / 5; // Reason: we multiply all Minute with 5 when reading the time
+    this.activeMinuteMap.clear();
+    this.setMappings(preparedMinute, false);
+  }
+
+  private setMappings(value: number, isHour: boolean): void {
+    if (value >= 0 && value <= 12) {
+      const activeFields = this.numberToBoxMap.get(value);
+      activeFields?.forEach(field => {
+        isHour ? this.activeHourMap.set(field, true) : this.activeMinuteMap.set(field, true);
+      })
+    }
+  }
 }
