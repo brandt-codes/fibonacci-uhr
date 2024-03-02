cab632c5c5369b444e321dd58d6601f82e46382e
Alexander Brandt <alex@brandt.codes>
2024-03-02 14:02:13 +0100

    feat: clock controls part 2: functionality

--- src/app/clock-control/clock-control.component.spec.ts
+++ src/app/clock-control/clock-control.component.spec.ts
@@ -2,9 +2,113 @@ import { ClockControlComponent } from './clock-control.component';
 
 describe('ClockControlComponent', () => {
   let component: ClockControlComponent;
+  beforeEach(() => {
+    component = new ClockControlComponent();
+  })
 
   it('should create', () => {
-    component = new ClockControlComponent();
     expect(component).toBeTruthy();
   });
+
+  describe('Change Hour', () => {
+    it('should set hour to selected value', () => {
+      component.onSelectHour({target: {value: '9'}});
+      expect(component.hour).toBe(9);
+    });
+
+    it('should set "displayHour" to selected value, without leading 0', () => {
+      component.onSelectHour({target: {value: '11'}});
+      expect(component.displayHour).toBe('11');
+    });
+
+    it('should add a leading 0 to "displayHour" if selected hour is < 10', () => {
+      component.onSelectHour({target: {value: '3'}});
+      expect(component.displayHour).toBe('03');
+    });
+  });
+
+  describe('Change Minute', () => {
+    it('should set minute to selected value', () => {
+      component.onSelectMinute({target: {value: '3'}});
+      expect(component.minute).toBe(3);
+    });
+
+    it('should set "displayMinute" to selected value, without leading 0', () => {
+      component.onSelectMinute({target: {value: '12'}});
+      expect(component.displayMinute).toBe('12');
+    });
+
+    it('should add a leading 0 to "displayMinute" if selected hour is < 10', () => {
+      component.onSelectMinute({target: {value: '4'}});
+      expect(component.displayMinute).toBe('04');
+    });
+  });
+
+  describe('Button - Plus', () => {
+      it('should add 5 minutes on button + action', () => {
+        component.minute = 10;
+        component.plus5Minutes();
+        expect(component.minute).toBe(15);
+      });
+
+    it('should reset minutes if overflow reached (55 + 5 -> 00)', () => {
+      component.minute = 55;
+      component.plus5Minutes();
+      expect(component.minute).toBe(0);
+    });
+
+    it('should also update displayMinute on plus 5 minutes action', () => {
+      component.minute = 50;
+      component.plus5Minutes();
+      expect(component.displayMinute).toBe('55');
+    });
+
+    it('should increment hour when minute overflow reached', () => {
+      component.hour = 1;
+      component.minute = 55;
+      component.plus5Minutes();
+      expect(component.hour).toBe(2);
+    });
+
+
+    it('should reset hour when hour overflow reached - on plus', () => {
+      component.hour = 12;
+      component.minute = 55;
+      component.plus5Minutes();
+      expect(component.hour).toBe(0);
+      expect(component.minute).toBe(0);
+    });
+  });
+
+  describe('Button - Minus', () => {
+    it('should subtract 5 minutes on button minus action', () => {
+      component.minute = 10;
+      component.minus5Minutes();
+      expect(component.minute).toBe(5);
+    });
+
+    it('should set minutes to 55 if 0 is reached (0 - 5 -> 55)', () => {
+      component.minute = 0;
+      component.minus5Minutes();
+      expect(component.minute).toBe(55);
+    });
+
+    it('should decrement hour when minute overflow reached', () => {
+      component.hour = 3;
+      component.minute = 0;
+      component.minus5Minutes();
+      expect(component.hour).toBe(2);
+    });
+
+    it('should reset hour when hour overflow reached - on minus', () => {
+      component.hour = 0;
+      component.minute = 0;
+      component.minus5Minutes();
+      expect(component.hour).toBe(12);
+      expect(component.minute).toBe(55);
+    });
+  });
+
+  // TODO AVB: call a Service - write current values to store!
+
 });
--- src/app/clock-control/clock-control.component.ts
+++ src/app/clock-control/clock-control.component.ts
@@ -1,13 +1,71 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-clock-control',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './clock-control.component.html',
   styleUrl: './clock-control.component.scss'
 })
 export class ClockControlComponent {
   public hour: number = 0;
   public minute: number = 0;
+  public displayHour: string = '00';
+  public displayMinute: string = '00';
+
+  public onSelectHour(event: any): void {
+    const newHour = Number(event?.target?.value);
+    if (newHour && newHour >= 0 && newHour <= 12) {
+      this.hour = newHour;
+      this.displayHour = this.getDisplayFormat(newHour);
+    }
+  }
+
+  public onSelectMinute(event: any): void {
+    const newMinute = Number(event?.target?.value);
+    if (newMinute && newMinute >= 0 && newMinute <= 55) {
+      this.minute = newMinute;
+      this.displayMinute = this.getDisplayFormat(newMinute);
+    }
+  }
+
+  public plus5Minutes(): void {
+    if (this.minute < 55) {
+      this.minute = this.minute + 5;
+    } else {
+      this.minute = 0;
+      if (this.hour < 12) {
+        this.hour++;
+      } else {
+        this.hour = 0;
+      }
+    }
+    this.updateDisplayValues();
+  }
+
+  public minus5Minutes(): void {
+    if (this.minute > 0) {
+      this.minute = this.minute - 5;
+    } else {
+      this.minute = 55;
+      if (this.hour > 0) {
+        this.hour--;
+      } else {
+        this.hour = 12;
+      }
+    }
+    this.updateDisplayValues();
+  }
+
+  private getDisplayFormat(num: number): string {
+    return num <= 9 ? `0${num.toString()}` : num.toString();
+  }
+
+  private updateDisplayValues(): void {
+    this.displayMinute = this.getDisplayFormat(this.minute);
+    this.displayHour = this.getDisplayFormat(this.hour);
+  }
 }
