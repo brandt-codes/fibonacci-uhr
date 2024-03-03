bdb3d496b5736e9935115add731f848db8d2e72f
Alexander Brandt <alex@brandt.codes>
2024-03-03 14:25:44 +0100

    feat: shell script to install, test and run the App locally

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Install dependencies:"
+npm i
+echo "Dependencies installed."
+
+echo "Run unit tests:"
+npm run test -- --bail
+if [ $? -ne 0 ]; then
+  echo "Unit test failed - Build canceled!"
+  exit 1
+
+else
+  echo "Unit tests done."
+  echo "Run build:"
+  npm run build:prod
+  echo "Build done."
+
+  read -p "Start local http-server (y/N)? " start_server
+
+  if [ "$start_server" = "y" ] || [ "$start_server" = "Y" ]; then
+    echo "x has the value 'valid'"
+    cd "dist/fibonacci-uhr/browser"
+    http-server
+  else
+    echo "DONE!"
+  fi
+fi
